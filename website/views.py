a2a8d0c333965e15990ba60318951351c9b474e8
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2024-07-02 22:19:36 -0300

    Register Users

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
   # Check to see if logging in
@@ -28,3 +29,20 @@ def logout_user(request):
   logout(request)
   messages.success(request, ('You have been logged out'))
   return redirect('home')
+
+def register_user(request):
+  if request.method == 'POST':
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password1')
+      user = authenticate(username = username, password = password)
+      login(request, user)
+      messages.success(request, "You Have Successfully Registered! Welcome!")
+      return redirect('home')
+  else:
+    form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
+
+  return render(request, 'register.html', {'form': form})
